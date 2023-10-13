94f2386cc2b3a096bc3b3d0bd6e6d50dea18e35b
Johanur Rahman <johanur.rahman@pm.me>
2023-10-13 21:43:54 +0600

    Server actions and components added for todo

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,13 @@
+import NewTodoForm from '@/components/NewTodoForm';
+
 const DashboardLayout = ({ children }) => {
   return (
     <div>
       <div>
         <h1>Todos</h1>
+      </div>
+      <div>
+        <NewTodoForm />
       </div>
       <div>{children}</div>
     </div>
--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,5 +1,17 @@
-const TodosPage = () => {
-  return <div>Todos Page</div>;
+import db from '@/utils/db';
+import TodoList from '@/components/TodoList';
+
+const getData = async () => {
+  return await db.todo.findMany({});
+};
+
+const TodosPage = async () => {
+  const todos = await getData();
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  );
 };
 
 export default TodosPage;
