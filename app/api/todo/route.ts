2fe73aa19652b37fcf0221f02e1bd1992456f876
Johanur Rahman <johanur.rahman@pm.me>
2023-10-14 02:20:35 +0600

    Api routes added

---
+++ app/api/todo/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import db from '@/utils/db';
+
+export const GET = async () => {
+  const data = await db.todo.findMany({});
+  return NextResponse.json({ data });
+};
+
+export const POST = async (request: Request) => {
+  const data = await request.json();
+  const todo = await db.todo.create({ data });
+  return NextResponse.json({ data: todo });
+};
